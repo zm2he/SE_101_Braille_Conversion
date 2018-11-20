f74f492b9679b7c8a41aec35ac000abe08ac9276
Ishan <iamlekar@uwaterloo.ca>
2018-11-21 05:15:42 +0530

    Corrections for the button program

--- Button_test.py
+++ Button_test.py
@@ -2,36 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.Board)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+#GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 """
 while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
+    if GPIO.input(11) == GPIO.HIGH:
         print("Button was pushed!")
 """
-
 """
 # using the callback method
-def button1_callback(channel)
+def button1_callback(channel):
         print("Hooray Button 1");
         
-def button2_callback(channel)
+def button2_callback(channel):
     print("Hooray Button 2");
     
 GPIO.add_event_detect(11, GPIO.RISING, callback=button1_callback)
-GPIO.add_event_detect(12, GPIO.Rising, callback=button2_callback)
+GPIO.add_event_detect(12, GPIO.RISING, callback=button2_callback)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
 """
 
-"""
+
 # returning input to our original functions
 # use global variables that are altered?
 # return function to output
 # call an auxiliary function that 
-"""
+
 
 """
 using the debounce method 
@@ -45,13 +44,13 @@ while True:
 # this is an endless for loop.
 
 """
-"""
-prev_input = TRUE
+
+prev_input = GPIO.LOW
 input = GPIO.input(11)
-while ((not prev_input) and input)
+while (prev_input ==GPIO.LOW and input != GPIO.HIGH):
     input = GPIO.input(11)
-    if ((not prev_input) and input)
+    if ((not prev_input) and input):
         print("button pressed")
     prev_input = input
     time.sleep(0.05)
-"""
+print("Test Successful")
